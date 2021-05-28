d4f0daf96d8a1d3da103f4cf2aa9444737bdd787
DESKTOP-AP3F894\pc <ffaggiano15@gmail.com>
2021-05-28 15:44:13 +0200

    ho eliminato getter e setter dai model

--- siw-museo/src/main/java/it/uniroma3/siw/museo/model/Collezione.java
+++ siw-museo/src/main/java/it/uniroma3/siw/museo/model/Collezione.java
@@ -39,46 +39,4 @@ public class Collezione {
 	public Collezione() {
 		this.opereContenute= new ArrayList<>();
 	}
-	/*
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getDescrizione() {
-		return descrizione;
-	}
-
-	public void setDescrizione(String descrizione) {
-		this.descrizione = descrizione;
-	}
-
-	public Curatore getCuratore() {
-		return curatore;
-	}
-
-	public void setCuratore(Curatore curatore) {
-		this.curatore = curatore;
-	}
-
-	public List<Opera> getOpereContenute() {
-		return opereContenute;
-	}
-
-	public void setOpereContenute(List<Opera> opereContenute) {
-		this.opereContenute = opereContenute;
-	}
-	
-	*/
 }
