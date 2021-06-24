f7534280f23fcd3b382fd872f85dc1baa2491db2
AleGates <72610707+AleGates@users.noreply.github.com>
2021-06-24 12:58:25 +0200

    ho eliminato le vecchie classi per gestire l'autenticazione e ho importato quelle del prof

--- siw-museo/src/main/java/it/uniroma3/siw/museo/authentication/AuthConfiguration.java
+++ siw-museo/src/main/java/it/uniroma3/siw/museo/authentication/AuthConfiguration.java
@@ -13,6 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
+import static it.uniroma3.siw.museo.model.Credentials.ADMIN_ROLE;
+//import static it.uniroma3.siw.spring.model.Credentials.DEFAULT_ROLE;
+
 /**
  * The AuthConfiguration is a Spring Security Configuration.
  * It extends WebSecurityConfigurerAdapter, meaning that it provides the settings for Web security.
@@ -36,27 +39,31 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter {
         http
                 // authorization paragraph: qui definiamo chi può accedere a cosa
                 .authorizeRequests()
-               //DA CONTROLLARE SE FUNZIONA ANCHE COSì
-                .antMatchers(HttpMethod.GET, "/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/**").permitAll()
-                //DA CONTROLLARE SE FUNZIONA ANCHE COSì
-                .antMatchers(HttpMethod.GET, "/admin/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/admin/**").authenticated()
+                // chiunque (autenticato o no) può accedere alle pagine index, login, register, ai css e alle immagini
+                .antMatchers(HttpMethod.GET, "/", "/index", "/login", "/register", "/css/**", "/images/**").permitAll()
+                // chiunque (autenticato o no) può mandare richieste POST al punto di accesso per login e register 
+                .antMatchers(HttpMethod.POST, "/login", "/register").permitAll()
+                // solo gli utenti autenticati con ruolo ADMIN possono accedere a risorse con path /admin/**
+                .antMatchers(HttpMethod.GET, "/admin/**").hasAnyAuthority(ADMIN_ROLE)
+                .antMatchers(HttpMethod.POST, "/admin/**").hasAnyAuthority(ADMIN_ROLE)
+                // tutti gli utenti autenticati possono accere alle pagine rimanenti 
+                .anyRequest().authenticated()
 
                 // login paragraph: qui definiamo come è gestita l'autenticazione
+                // usiamo il protocollo formlogin 
                 .and().formLogin()
                 // la pagina di login si trova a /login
                 // NOTA: Spring gestisce il post di login automaticamente
                 .loginPage("/login")
                 // se il login ha successo, si viene rediretti al path /default
-                .defaultSuccessUrl("/admin/home")
+                .defaultSuccessUrl("/default")
 
                 // logout paragraph: qui definiamo il logout
                 .and().logout()
                 // il logout è attivato con una richiesta GET a "/logout"
                 .logoutUrl("/logout")
                 // in caso di successo, si viene reindirizzati alla /index page
-                .logoutSuccessUrl("/home")        
+                .logoutSuccessUrl("/index")        
                 .invalidateHttpSession(true)
                 .clearAuthentication(true).permitAll();
     }
@@ -70,9 +77,9 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter {
                 //use the autowired datasource to access the saved credentials
                 .dataSource(this.datasource)
                 //retrieve username and role
-                .authoritiesByUsernameQuery("SELECT username, role FROM admin WHERE username=?")
+                .authoritiesByUsernameQuery("SELECT username, role FROM credentials WHERE username=?")
                 //retrieve username, password and a boolean flag specifying whether the user is enabled or not (always enabled in our case)
-                .usersByUsernameQuery("SELECT username, password, 1 as enabled FROM admin WHERE username=?");
+                .usersByUsernameQuery("SELECT username, password, 1 as enabled FROM credentials WHERE username=?");
     }
 
     /**
