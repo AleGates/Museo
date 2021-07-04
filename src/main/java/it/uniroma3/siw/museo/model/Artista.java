8467f2e4865f41be617d6721c00abfb9a032a9ad
AleGates <72610707+AleGates@users.noreply.github.com>
2021-07-04 18:07:11 +0200

    eliminata cartella siw-museo, spostato tutto nella root

---
+++ src/main/java/it/uniroma3/siw/museo/model/Artista.java
@@ -0,0 +1,62 @@
+package it.uniroma3.siw.museo.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class Artista {
+
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+
+	@Column (nullable=false)
+	private String nome;
+
+	@Column (nullable=false)
+	private String cognome;
+
+	@Column (nullable=false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dataNascita;
+	
+	@Column (nullable=false)
+	private String luogoNascita;
+	
+	@Column (nullable=false)
+	private String nazionalita;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dataMorte;
+	
+	private String luogoMorte;
+	
+	@Column (length=2000)
+	private String biografia;
+
+
+
+	@OneToMany(mappedBy="artista", cascade = CascadeType.ALL)
+	List<Opera> opereRealizzate;
+
+
+	public Artista(){
+
+	}
+}
---
+++ src/main/java/it/uniroma3/siw/museo/model/Curatore.java
@@ -0,0 +1,51 @@
+package it.uniroma3.siw.museo.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class Curatore {
+
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column (nullable=false)
+	private String nome;
+	
+	@Column (nullable=false)
+	private String cognome;
+
+	@Column (unique=true, nullable=false)
+	private Long matricola;
+	
+	private LocalDate dataDiNascita;
+	
+	private String luogoDiNascita;
+	
+	private String email;
+	
+	private Long numeroTelefono;
+	
+	@OneToMany(mappedBy="curatore")
+	List<Collezione> collezioniCurate;
+	
+	public Curatore() {
+		collezioniCurate = new ArrayList<>();
+	}
+}
